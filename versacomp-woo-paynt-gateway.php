35c52e486b11192264150a670196bb8919715544
Mitch Lipayon <mitch@ackerwines.com>
2021-08-04 23:43:35 -0700

    Create plugin file

---
+++ versacomp-woo-paynt-gateway.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: VersaComp WooCommerce PAYNT Gateway
+Plugin URI: https://www.versacomputer.com/woo-paynt/
+Description: Choice Payment gateway for WooCommerce using PAYNT API.
+Version: 1.0.0
+WC tested up to: 5.7.2
+Author: Versa Computer
+Author URI: https://www.versacomputer.com/woo-paynt/
+*/
+
+class VersaCompPayntGateway {
+    //TODO: create plugin class
+}
+new VersaCompPayntGateway();
